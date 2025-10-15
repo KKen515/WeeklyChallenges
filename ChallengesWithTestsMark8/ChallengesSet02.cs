643f4102f9ba06aa5050c3ffe6c424705c3f2bc6
Kenton Knelle <kken51915@gmail.com>
2025-10-14 23:12:52 -0400

    Weekly Challenges Set 02 Push

--- ChallengesWithTestsMark8/ChallengesSet02.cs
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.JavaScript;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,52 +9,163 @@ namespace ChallengesWithTestsMark8
     {
         public bool CharacterIsALetter(char c)
         {
-            throw new NotImplementedException();
+            string alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                if (alphabet[i] == c)
+                {
+                    return true;
+                }
+
+
+            } return false;
         }
+
+
+        //return char.IsLetter(c);
+        
 
         public bool CountOfElementsIsEven(string[] vals)
         {
-            throw new NotImplementedException();
+            return vals.Length % 2 == 0;
         }
 
         public bool IsNumberEven(int number)
         {
-            throw new NotImplementedException();
+            if (number % 2 == 0)
+            {
+                return true;
+            }
+           return false;
         }
 
         public bool IsNumberOdd(int num)
         {
-            throw new NotImplementedException();
+            return num % 2 != 0;
         }
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
-            throw new NotImplementedException();
+
+          /*  if (numbers == null || numbers.Count() <= 0)
+            {
+                return 0;
+            }
+            
+            double  min = 0;
+            double   max = 0;
+            double currentMin = 0;
+            double currentMax = 0;
+            
+            var list= numbers.ToList();
+
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                if (list[i] > list[i + 1])
+                {
+                    max = list[i];
+                }
+            }
+
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                if (list[i] < list[i + 1])
+                {
+                    min = list[i];
+
+                }
+                
+            }
+            
+
+            return min + max;
+            
+            
+            */
+            return numbers == null || numbers.Count() == 0 ? 0 : numbers.Min() + numbers.Max();
+
+
+
         }
 
         public int GetLengthOfShortestString(string str1, string str2)
         {
-            throw new NotImplementedException();
+            if (str2.Length < str1.Length)
+            {
+                return str2.Length;
+            }
+            
+            return str1.Length;
+
         }
 
         public int Sum(int[] numbers)
         {
-            throw new NotImplementedException();
+            
+            if (numbers == null || numbers.Count() <= 0)
+            {
+                return 0;
+            }
+            
+            int sum = 0;
+            foreach (var number in numbers)
+            {
+                sum += number;
+            }
+            return sum;
         }
 
         public int SumEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            int sum = 0;
+            
+            if (numbers == null || numbers.Count() <= 0)
+            {
+                return 0;
+            }
+            
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    sum += numbers[i];
+                }
+            }
+
+            return sum;
         }
 
         public bool IsSumOdd(List<int> numbers)
         {
-            throw new NotImplementedException();
+
+            if (numbers == null)
+            {
+                return false;
+            }
+
+            return numbers.Sum() % 2 != 0;
         }
+
 
         public long CountOfPositiveOddsBelowNumber(long number)
         {
-            throw new NotImplementedException();
+            var count = 0;
+
+            if (number == 1 || number <= 0)
+            {
+                return 0;
+            }
+
+            for (long i = number; i > 0; i--)
+            {
+                if (i % 2 == 0)
+                {
+                    count++;
+                }
+            } return count;
+            
         }
+        
     }
 }
